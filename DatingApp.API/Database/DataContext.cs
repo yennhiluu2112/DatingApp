6e9b1a323fdf5970d7fc666faba73b2ca00b3e0a
yennhiluu2112 <yennhiluu2112@gmail.com>
2022-09-16 13:34:38 +0700

    nhi cm 1:46:19

---
+++ DatingApp.API/Database/DataContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DatingApp.DatingApp.API.Database.Entities;
+
+namespace DatingApp.DatingApp.API.Database
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        
+        public DbSet<User> Users { get; set; }
+    }
+}
--- DatingApp.API/Database/Entities/User.cs
+++ DatingApp.API/Database/Entities/User.cs
@@ -2,11 +2,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DatingApp.DatingApp.API.Database.Entities
 {
+    [Table("User")]
     public class User
     {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(32)]
+        [Required]
+        public string UserName { get; set; }
+
+        [StringLength(255)]
+        [Required]
+        public string Email { get; set; }
         
     }
 }
